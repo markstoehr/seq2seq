0fb44c5ae8d8743b357854549e7df9d58dd4523a
Marcel Bollmann <bollmann@linguistics.rub.de>
2016-02-05 18:30:44 +0100

    Relax version requirements -- messes with my dependencies

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 from setuptools import find_packages
 
 install_requires = [
-    'numpy==1.9.2',
-    'Theano==0.7.0',
-    'Keras==0.3.0'
+    'numpy>=1.9.2',
+    'Theano>=0.7.0',
+    'Keras>=0.3.0'
 ]
 
 setup(
